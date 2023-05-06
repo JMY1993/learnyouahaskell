d9c1b0791b1dbb9793d7ed38dfb66c9fd24e3d28
JMY1993 <yangmohan1993@163.com>
2023-05-07 00:04:57 +0800

    Minor changes to guessNumber.hs

--- learnRandom/guessNumber.hs
+++ learnRandom/guessNumber.hs
@@ -16,11 +16,15 @@ askForNumber :: StdGen -> IO ()
 askForNumber gen = do
     putStrLn "Which number in the range from 1 to 10 am I thinking of?"
     input <- getLine
-    let (key, gen') = randomR (1,10) gen
-        guess = read input :: Int
+    let (key, gen') = randomR (1,10) gen :: (Int, StdGen)
+        parse = case reads input of
+            [] -> Nothing
+            [(guess, _)] -> Just guess
     -- gen' <- newStdGen
     unless (null input) $ do
-        if key == guess
-            then putStrLn "You are correct!"
-            else putStrLn ("Sorry, it was " ++ show key)
+        case parse of
+            Nothing -> putStrLn "Please enter a number"
+            (Just guess) -> if key == guess
+                                then putStrLn "You are correct!"
+                                else putStrLn ("Sorry, it was " ++ show key)
         askForNumber gen'
