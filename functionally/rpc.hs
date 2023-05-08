87b541e6f5e29d511e6f51511a6440bc4f77cd84
JMY1993 <yangmohan1993@163.com>
2023-05-08 23:20:15 +0800

    modified reverse polish calculator

--- functionally/rpc.hs
+++ functionally/rpc.hs
@@ -2,18 +2,23 @@
 requirements: we are building a reverse polish calculator, which will take a string like "10 4 3 + 2 * -" and calculate it
 
 The solution is a personal solution, which works well. But it's type declaration isn't `(Num a) => String -> a` as required.
+This is because in order to be read as a value, the value must have a type that is part of typeclass "Read", which we can manually add to it's declaration
+like this:
+(Num a, Read a) => String -> a
+
+so far so good
 -}
 
--- rpc :: (Num a) => String -> a
 rpc :: String -> Int
-rpc exp = case foldl process [] $ words exp of [result] -> result
+rpc = head . foldl process [] . words
 
 -- process :: (Num a) => [a] -> String -> [a]
-process :: [Int] -> String -> [Int]
-process stack [] = stack
+process :: (Num a, Read a) => [a] -> String -> [a]
+process stack "" = stack
 process (a1 : a2 : rest) x =
     case getOperator x of Nothing -> read x : a1 : a2 : rest
                           (Just op) -> op a2 a1 : rest
+-- remember to add this pattern at the bottom, since if not, you can't catch when the stack is empty or with less than two elements when the parameter `exp` isn't null
 process stack x = read x : stack
 
 getOperator :: (Num a) => String -> Maybe (a -> a -> a)
@@ -25,3 +30,15 @@ getOperator exp = lookup exp dispatch
         ("-", (-)),
         ("*", (*))
       ]
+
+-- below is a solution by the textbook
+solveRPN :: (Num a, Read a, Floating a) => String -> a  
+solveRPN = head . foldl foldingFunction [] . words  
+    where   foldingFunction (x:y:ys) "*" = (x * y):ys  
+            foldingFunction (x:y:ys) "+" = (x + y):ys  
+            foldingFunction (x:y:ys) "-" = (y - x):ys
+            foldingFunction (x:y:ys) "/" = (y / x):ys
+            foldingFunction (x:y:ys) "^" = (y ** x):ys  
+            foldingFunction (x:xs) "ln" = log x:xs  
+            foldingFunction xs "sum" = [sum xs]  
+            foldingFunction xs numberString = read numberString:xs
