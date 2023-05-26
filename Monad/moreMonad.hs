a51c92d9853b3d0d5f1f8087aec105ba7088628b
JMY1993 <yangmohan1993@163.com>
2023-05-26 15:31:53 +0800

    fix case (step1)

--- Monad/moreMonad.hs
+++ Monad/moreMonad.hs
@@ -2,3 +2,18 @@
 in this sourcefile, a series of function working on monadic values are covered
 -}
 
+{-
+we have a few tasks in this cource file:
+
+1. implement the monadic type Writer, as understand what an inefficient list construction is
+2. implement the difference list
+3. implement the Reader type
+4. [x] implement the stateful computation Monad (done in "./StateMonad.hs")
+5. wrap error in Either
+6. implement function liftM
+7. implement function join
+8. implement function filterM
+9. implement foldM
+10. make a safe RPN calculator
+11. learn how to make monads
+-}
--- monad/moreMonad.hs
+++
@@ -1,4 +0,0 @@
-{-
-in this sourcefile, a series of function working on monadic values are covered
--}
-
